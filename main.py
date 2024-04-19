352ff291a5e2429ee2390709fa7ae14c16b9c53a
alice white <mac@MACs-MacBook-Air.local>
2024-04-19 11:26:07 +0300

    Update

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ def main () -> None:
     path: str = input("Enter path >> ");
     assert os.path.exists(path), "DEBIL! NET PAPKI!";
     assert os.path.isdir(path), "THIS IS NOT A FOLDER DEBIL";
+    if (path[-1] != "/"):
+        path += "/"
     choice: str = input("boot/force? >> ");
     assert (choice in ("boot", "force")), "Da blyat, boot or force?";
     unit = Renamer();
-    unit.renameAllFiles("/Users/mac/Documents/project-codes/Python/4raven/", choice);
+    unit.renameAllFiles(path, choice);
 
 if (__name__ == "__main__"):
     main();
